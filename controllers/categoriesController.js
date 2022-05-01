0744f32c52d5de3c36b788c419eec6b5990428b8
glory <gauthier.lory@protonmail.com>
2022-05-01 14:56:01 +0200

    category get

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -11,4 +11,16 @@ module.exports = {
         const categories = await categoryDataMapper.findAllCategories();
         response.json({ data: categories });
     },
+
+    async categoryById (request, response) {
+        const { categoryId } = request.params;
+        const categories = await categoryDataMapper.findCategoryById(categoryId)
+
+        // Si post vaut null ou undefined
+        if (!categories) {
+            response.status(404).json({ error: "Not found" });
+        } else {
+            response.json({ data: categories });
+        }
+    }
 }
